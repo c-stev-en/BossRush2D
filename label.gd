a58d4dfd66bbd8507b571d396693888a241a2556
chair <csteven@sfsu.edu>
2025-01-18 00:35:01 -0800

    Let you see the value

---
+++ label.gd
@@ -0,0 +1,14 @@
+extends Label
+
+@onready var boss : CharacterBody2D = get_tree().\
+	get_first_node_in_group("boss")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (text != str(boss.value)):
+		text = str(boss.value)
